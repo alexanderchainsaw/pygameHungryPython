60258bc5ff20ce63b56cc3d309abcaec96248ada
alexanderchainsaw <alexanderkolesov.contact@gmail.com>
2023-08-20 08:13:46 +0300

    settings class init done
    all mobile variables of the higher-level settings class are set

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from base_settings import BaseSettings
+import pygame
 
 
 class Settings(BaseSettings):
@@ -7,9 +8,22 @@ class Settings(BaseSettings):
         """Gather lower level settings from the parent class"""
         super().__init__()
 
-        # variables to handle in-game scenarios:
+        # Variables to handle in-game scenarios:
+        # 1. Overall states of the game
         self.running, self.won, self.lost = False, False, False
 
-        # to allow only one movement button press per game tick:
+        # 2. To allow only one movement button press per game tick
         self.button_pressed = False
 
+        # 3. Level-up related variables
+        self.score, self.lvl, self.streak = 0, 0, 0
+
+        # Other variables:
+        # 1. Current set of obstacles according to current lvl
+        self.obstacles = self.levels[self.lvl]
+
+        # 2. Current speed
+        self.speed = self.base_speed
+
+        # 3. (x, y) snake position to manage directional movement
+        self.pos = (1, 0)
